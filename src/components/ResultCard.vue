b4093aa6cb0a691c2cd841bc1bbfbd10dd318f71
RilDev <c.public@tutanota.com>
2021-03-25 11:32:27 +0100

    create ResultCard

---
+++ src/components/ResultCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <li
+    class="flex bg-gray-50 hover:bg-gray-100 mb-3 px-4 py-3 rounded-md shadow-md sm:mb-5"
+  >
+    <img
+      class="w-20 h-20 rounded-full"
+      src="https://reqres.in/img/faces/1-image.jpg"
+      alt=""
+      width="384"
+      height="512"
+    />
+    <div class="ml-5 flex flex-col justify-between">
+      <div class="text-xl font-bold">George Bluth</div>
+      <a
+        href="mailto:george.bluth@reqres.in"
+        class="text-blue-600 hover:text-blue-500"
+        >george.bluth@reqres.in</a
+      >
+    </div>
+  </li>
+</template>
